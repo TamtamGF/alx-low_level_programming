435cb59327bb813a75d0a06e6b41c3d143040d55
TamtamGF <samuelochieng033@gmail.com>
2023-06-21 16:10:04 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @k:int to check
+ * Return: the absolute value of int
+ */
+int _abs(int k)
+{
+	if (k >= 0)
+	{
+		return (k);
+	}
+	return (-k);
+}
+
