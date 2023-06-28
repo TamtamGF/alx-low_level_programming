db16a0dff71fd50252c86d2cd029958ada62263e
TamtamGF <samuelochieng033@gmail.com>
2023-06-27 17:47:51 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - switch two integers
+ * @x: integer one
+ * @z: integer two
+ *
+ * return: void
+ */
+
+void swap_int(int *x, int *z)
+{
+	int tmp = *x;
+	*x = *z;
+	*b = tmp;
+}
