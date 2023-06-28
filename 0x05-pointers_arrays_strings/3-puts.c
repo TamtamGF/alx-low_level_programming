37d14d1f17ec6259ca2acaa113ea55f048f3067c
TamtamGF <samuelochieng033@gmail.com>
2023-06-27 18:18:42 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - output string to stdout
+ * @s: pointer to string
+ * return: void
+ */
+
+void _puts(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; i++)
+		_putchar(s[n]);
+	_putchar('\n');
+}
