a239de11a4c9577b2de9318865b31d974564e463
TamtamGF <samuelochieng033@gmail.com>
2023-06-27 19:18:54 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,47 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int nb;
+* _atoi - convert a string to an integer
+* @s: char
+* Return: int converted from string
+*/
 
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
+int _atoi(char *s)
+{
+int a, d, o, len, r, digit;
+
+a = 0;
+d = 0;
+o = 0;
+len = 0;
+r = 0;
+digit = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (a < len && r == 0)
+{
+if (s[a] == '-')
+++d;
+
+if (s[a] >= '0' && s[a] <= '9')
+{
+digit = s[a] - '0';
+if (d % 2)
+digit = -digit;
+o = o * 10 + digit;
+r = 1;
+if (s[a + 1] < '0' || s[a + 1] > '9')
+break;
+r = 0;
+}
+a++;
+}
+
+if (r == 0)
+return (0);
+
+return (o);
+
 }
