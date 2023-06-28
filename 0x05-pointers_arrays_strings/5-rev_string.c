5af8db953173cd338a27f8466c78609533203bf2
TamtamGF <samuelochieng033@gmail.com>
2023-06-27 18:36:30 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - codes  that reverses string
+ * @k: string pointer
+ * Return: 0
+ */
+void rev_string(char *k)
+{
+	int i = 0;
+	int l = 0;
+	char temp;
+
+	while (k[i++])
+	{
+		l++;
+	}
+	for (i = l - 1; i >= l / 2; i--)
+	{
+		temp = k[i];
+		s[i] = k[l - i - 1];
+		k[l - i - 1] = temp;
+	}
+}
