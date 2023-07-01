dd73d6d7bd7a1922c24a99cb75dd9b04b8eff994
TamtamGF <samuelochieng033@gmail.com>
2023-06-30 18:13:32 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest:char
+ *  @src:char
+ * @n:int
+ * Return:char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int k;
+
+k = 0;
+	while (k < n && *(src + k))
+	{
+	*(dest + k) = *(src + k);
+	k++;
+	}
+	while (k < n)
+	{
+	*(dest + k) = '\0';
+	k++;
+	}
+	return (dest);
+}
